eb22194a20a02c22000a4541e34e785117f9de75
Whitney <foster.whitney.a@gmail.com>
2019-03-21 22:58:40 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(Hello) {
+  return Hello.toUpperCase()
+}
+
+function whisper(HELLO) {
+  return HELLO.toLowerCase()
+}
+function logShout(Hello) {
+  console.log(Hello.toUpperCase);
+}
+function logWhisper(HELLO)
+  console.log(HELLO.toLowerCase);
+}
+function sayHiToGrandma(Hello) {
+  if (Hello.tolowercase)
+  return ("I can\'t hear you!")
+
+  if (Hello.toUpperCase)
+  return ("YES INDEED!")
+
+  if else sayHiToGrandma(I love you) {
+    return ("I love you too")
