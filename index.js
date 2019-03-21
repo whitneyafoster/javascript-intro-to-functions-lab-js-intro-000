1586774ea9259d4ce78fbdbd5df687a7190c9b54
Whitney <foster.whitney.a@gmail.com>
2019-03-21 23:29:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ function sayHiToGrandma(string) {
 }
 
   function sayHiToGrandma(string) {
-    if (string ==== "I love you, Grandma.")
+    if (string === "I love you, Grandma.")
     return "I love you, too."
 }
