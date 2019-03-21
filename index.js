088d383faaf71047fd4d1fe275267ac62adc2675
Whitney <foster.whitney.a@gmail.com>
2019-03-21 23:44:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 function shout(Hello) {
   return Hello.toUpperCase()
 }
-
 function whisper(Hello) {
   return Hello.toLowerCase()
 }
-
 function logShout(Hello) {
   console.log(Hello.toUpperCase());
-
 }
-
 function logWhisper(Hello) {
   console.log(Hello.toLowerCase());
 }
-
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
   return 'I can\'t hear you!'
 }
-
 function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
   return 'YES INDEED!'
